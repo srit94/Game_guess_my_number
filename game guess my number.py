09cfbacc341542efe747f1c3ae8d9d771f0b8c48
srit94 <r.sergej94@gmail.com>
2023-01-11 10:15:32 +0100

    Update game guess my number.py
    - Break the game when input is 0.
    - Optimize conditions for hints
    - success message only apears when user wins

--- game guess my number.py
+++ game guess my number.py
@@ -5,19 +5,25 @@ print("Computer überlegt sich eine Zahl...")
 randInteger = random.randrange(1, 1000)
 
 print("Fertig. Ich habe mir eine Zahl zwischen 1 - 1000 überlegt")
+print("Sie können das Spiel jederzeit mit der Eingabe '0' beenden.")
 
-guessedNumber = 0
+guessedNumber = -1
 tries = 0
 
 while randInteger != guessedNumber:
     guessedNumber = int( input("Errate die Zahl: ") )
 
+    #stop the game
+    if guessedNumber == 0:
+        print("Das Spiel wird abgebrochen")
+        break;
+
+    #give a hint
     if guessedNumber > randInteger:
         print("Deine Zahl ist größer als meine.")
-
-    if guessedNumber < randInteger:
+    elif guessedNumber < randInteger:
         print("Deine Zahl ist kleiner als meine.")
 
     tries += 1
-
-print("Super. Du hast meine Zahl erraten", randInteger, ". Du hast", tries, "Versuche gebraucht.")
+else:
+    print("Super. Du hast meine Zahl erraten", randInteger, ". Du hast", tries, "Versuche gebraucht.")
